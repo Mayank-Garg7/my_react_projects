260b8df9cd006be00e3c5f0024c88ff94d3a5f46
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-05-21 15:08:47 +0530

    make the code more concise

--- To_Do_App/src/components/ToDos_Form.tsx
+++ To_Do_App/src/components/ToDos_Form.tsx
@@ -3,63 +3,59 @@ import Button from "./shared/Button";
 import { useState } from "react";
 
 function ToDos_Form() {
-  const [text, setText] = useState("");
-  const [showMessage, setShowMessage] = useState(false);
+    const [text, setText] = useState("");
+    const [showMessage, setShowMessage] = useState(false);
 
-  const handleTextChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const value = event.target.value;
+    const handleTextChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        const value = event.target.value;
 
-    if (value !== "" && value.trim().length < 10) {
-      setShowMessage(true);
-    } else {
-      setShowMessage(false);
-    }
+        setShowMessage(value.trim().length > 0 && value.trim().length < 10);
 
-    setText(value);
-  };
+        setText(value);
+    };
 
-  const handleSubmit = (
-    event: React.FormEvent<HTMLFormElement>
-  ) => {
-    event.preventDefault();
+    const handleSubmit = (
+        event: React.FormEvent<HTMLFormElement>
+    ) => {
+        event.preventDefault();
 
-    console.log(text);
-  };
+        console.log(text);
+    };
 
-  return (
-    <Card>
-      <div className="flex gap-3 flex-col">
-        <h1 className="m-auto">
-          What are you thinking to complete next.
-        </h1>
+    return (
+        <Card>
+            <div className="flex gap-3 flex-col">
+                <h1 className="m-auto">
+                    What are you thinking to complete next.
+                </h1>
 
-        <form
-          onSubmit={handleSubmit}
-          className="flex gap-3 flex-col"
-        >
-          <input
-            type="text"
-            value={text}
-            onChange={handleTextChange}
-            className="bg-gray-100 w-3/4 m-auto p-2 rounded-lg text-black font-bold"
-            placeholder="what are you thinking to do next"
-          />
+                <form
+                    onSubmit={handleSubmit}
+                    className="flex gap-3 flex-col"
+                >
+                    <input
+                        type="text"
+                        value={text}
+                        onChange={handleTextChange}
+                        className="bg-gray-100 w-3/4 m-auto p-2 rounded-lg text-black font-bold"
+                        placeholder="what are you thinking to do next"
+                    />
 
-          {showMessage && (
-            <p className="text-xl m-auto my-1">
-              😉 Think more dude!
-            </p>
-          )}
+                    {showMessage && (
+                        <p className="text-xl m-auto my-1">
+                            😉 Think more dude!
+                        </p>
+                    )}
 
-          <Button type="submit">
-            Submit
-          </Button>
-        </form>
-      </div>
-    </Card>
-  );
+                    <Button type="submit">
+                        Submit
+                    </Button>
+                </form>
+            </div>
+        </Card>
+    );
 }
 
 export default ToDos_Form;
