5cf4673fa372e81f3aaf8f0678ffeaccac24b898
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-05-21 15:14:13 +0530

    chore: enhance the ui of card

--- To_Do_App/src/components/shared/Card.tsx
+++ To_Do_App/src/components/shared/Card.tsx
@@ -6,7 +6,7 @@ type CardProps = {
 
 function Card({ children }: CardProps) {
   return (
-    <div className="bg-cyan-950 text-white px-2 py-2 font-bold text-xl">
+    <div className="bg-cyan-950 text-white px-2 py-2 font-bold text-xl w-2/4 justify-self-center-safe rounded-lg">
       {children}
     </div>
   );
