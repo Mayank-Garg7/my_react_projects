e1b8446e51e8963b2ba847fbfa0fc7382f1f5336
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-05-21 16:07:15 +0530

    feat: inhamce the logics

--- To_Do_App/src/context/Context_Work.tsx
+++ To_Do_App/src/context/Context_Work.tsx
@@ -1,12 +1,31 @@
-import {createContext} from 'react';
+import { createContext, type ReactNode } from 'react';
 
 
-function Context_Work() {
-  return (
-    <div>
-      
-    </div>
-  )
+type ContextType = {
+  add_To_Work: (work: string) => void;
+};
+const Context_Work = createContext<ContextType | null>(null);
+
+
+type ChildrenProps = {
+    children: ReactNode
+}
+
+
+export const ContextProvider = ({ children }: ChildrenProps) => {
+    const add_To_Work = (work : string) => {
+        console.log(work)
+    }
+
+
+    return (
+        <Context_Work.Provider value={{
+            add_To_Work,
+
+        }}>
+            {children}
+        </Context_Work.Provider>
+    )
 }
 
 export default Context_Work
