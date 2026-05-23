5f6cd2a67f5263164b89bce41d5c4f4425a4f4a9
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-05-23 18:01:23 +0530

    chore: update the json by adding status and context as well

--- To_Do_App/src/context/Context_Work.tsx
+++ To_Do_App/src/context/Context_Work.tsx
@@ -4,8 +4,8 @@ import Tasks from '../Data/Tasks.json'
 
 
 type ContextType = {
-  work: Task[];
-  add_To_Work: (text: string) => void;
+    work: Task[];
+    add_To_Work: (text: string) => void;
 };
 const Context_Work = createContext<ContextType | null>(null);
 
@@ -16,10 +16,21 @@ type ChildrenProps = {
 export type Task = {
     id: number;
     text: string;
+    status: "pending" | "completed";
 };
 
 
 export const ContextProvider = ({ children }: ChildrenProps) => {
+    const [tasks, setTasks] = useState<Task[]>(data);
+
+    const updateTaskStatus = (id: number, status: "pending" | "completed") => {
+        setTasks((prev) =>
+            prev.map((task) =>
+                task.id === id ? { ...task, status } : task
+            )
+        );
+    };
+
 
     const [work, setWork] = useState<Task[]>(() => {
         const data = localStorage.getItem("Work_to_do");
@@ -31,7 +42,7 @@ export const ContextProvider = ({ children }: ChildrenProps) => {
 
 
     const add_To_Work = (text: string) => {
-        const newTask : Task = {
+        const newTask: Task = {
             id: new Date().getSeconds(),
             text: text,
         }
@@ -47,7 +58,7 @@ export const ContextProvider = ({ children }: ChildrenProps) => {
         }}>
             {children}
         </Context_Work.Provider>
-    )   
+    )
 }
 
 export default Context_Work
