ff3feed2c15af5d1921bae67f0d16f6cb0ae9aff
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-05-23 17:53:34 +0530

    feat: add the current status for the particullar task

--- To_Do_App/src/components/Show_Task.tsx
+++ To_Do_App/src/components/Show_Task.tsx
@@ -7,13 +7,24 @@ type ShowTaskProps = {
 };
 
 function Show_Task({ item }: ShowTaskProps) {
+  const handleStatusChange = (status: string) => {
+    console.log(status);
+  }
   return (
     <Card>
       <p>{item.text}</p>
       <FaTimes className="absolute right-3 top-3 text-red-600"/>
       <FaUserEdit className="absolute right-10 top-3 text-green-600"/>
       <span>Status: {item.status}</span>
-
+      <select 
+      className="absolute right-3 text-black"
+      value={item.status}
+      onChange={(e) => handleStatusChange(e.target.value)}
+      >
+        <option value="status">status</option>
+        <option value="pending">pending</option>
+        <option value="completed">completed</option>
+      </select>
     </Card>
   );
 }
