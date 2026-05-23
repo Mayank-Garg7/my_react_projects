801c0306a465e84e61ff1b3e20f22be30880bc32
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-05-23 17:34:01 +0530

    chore: update the file by making it clean

--- To_Do_App/src/components/Show_Task.tsx
+++ To_Do_App/src/components/Show_Task.tsx
@@ -1,6 +1,6 @@
 import type { Task } from "../context/Context_Work";
 import Card from "./shared/Card";
-
+import {FaTimes} from "react-icons/fa"
 type ShowTaskProps = {
   item: Task;
 };
@@ -9,6 +9,7 @@ function Show_Task({ item }: ShowTaskProps) {
   return (
     <Card>
       <p>{item.text}</p>
+      <FaTimes />
     </Card>
   );
 }
