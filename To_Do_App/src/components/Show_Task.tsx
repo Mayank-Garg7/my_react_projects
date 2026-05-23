2eb77c1732e3982517dd776b0b4cf16e66d68fee
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-05-23 17:49:13 +0530

    Add icons for task actions in Show_Task component

--- To_Do_App/src/components/Show_Task.tsx
+++ To_Do_App/src/components/Show_Task.tsx
@@ -1,5 +1,7 @@
 import type { Task } from "../context/Context_Work";
 import Card from "./shared/Card";
+import { FaTimes, FaUserEdit } from "react-icons/fa";
+
 type ShowTaskProps = {
   item: Task;
 };
@@ -8,6 +10,10 @@ function Show_Task({ item }: ShowTaskProps) {
   return (
     <Card>
       <p>{item.text}</p>
+      <FaTimes className="absolute right-3 top-3 text-red-600"/>
+      <FaUserEdit className="absolute right-10 top-3 text-green-600"/>
+      <span>Status: {item.status}</span>
+
     </Card>
   );
 }
