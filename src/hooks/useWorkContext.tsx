b20d8f2905f012b9a024743b04b18001f95d9c26
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-06-11 14:44:44 +0530

    chore: modifying the files

--- src/hooks/useWorkContext.tsx
+++
@@ -1,8 +0,0 @@
-import TodoContext from "../context/TodoContext";
-import { useContext } from "react";
-
-export const useWorkContext = () => {
-  const context = useContext(TodoContext);
-  if (!context) throw new Error("No Provider");
-  return context;
-};
