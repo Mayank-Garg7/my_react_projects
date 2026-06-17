4d303439bc047bf4c0c401cf0ef76d01c7a628f0
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-06-17 18:53:36 +0530

    updated the schema

--- backend/app/schemas/task.py
+++ backend/app/schemas/task.py
@@ -1,15 +1,12 @@
 from pydantic import BaseModel
 
-
 class TaskBase(BaseModel):
     text: str
     status: str
     priority: str
 
-
 class TaskCreate(TaskBase):
     pass
-
 
 class TaskResponse(TaskBase):
     id: int
